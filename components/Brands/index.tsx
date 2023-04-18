44b76c3763df1ef1cb5caa4f472eed9901ea21cd
Jayden Crowther <jaydencrowther@gmail.com>
2023-04-18 15:06:47 -0600

    images n shiz

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 const brandsData: Brand[] = [
   {
     id: 1,
-    name: "UIdeck",
-    href: "https://uideck.com",
-    image: "/images/brands/uideck.svg",
+    name: "Next JS",
+    href: "",
+    image: "/nextjslogo.png",
   },
   {
     id: 2,
     name: "Tailgrids",
     href: "https://tailgrids.com",
-    image: "/images/brands/tailgrids.svg",
+    image: "/MongoDB_Logoo.png",
   },
   {
     id: 3,
--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -71,7 +71,7 @@ const Video = () => {
                 <form className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0 m-16" onSubmit={uploadEmail}>
                   <input value={email} onChange={(e) => {setEmail(e.target.value)}} type="email" placeholder="Email" className="rounded-md py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out dark:text-black dark:bg-white border-2 border-primary" />
                   <button type="submit" className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/30 dark:bg-primary dark:text-white dark:hover:bg-white/30">
-                    Book my full demo
+                    Get early access!
                   </button>
                 </form> 
 
